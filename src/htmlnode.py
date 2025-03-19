7097ec9577e0ffad7b2f70688498a543daf9b865
marhjd <aussenstelle+github@posteo.de>
2025-03-19 10:59:09 +0000

    Refactor structure of nodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -21,3 +21,40 @@ class HTMLNode:
         if self.children is not None:
             children = "".join(str(child) for child in self.children)
         return f'<{self.tag}{self.props_to_html()}>{self.value}{children}</{self.tag}>'
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, children=None, props=props)
+
+
+    def to_html(self) -> str:
+        if self.value is None:
+            raise ValueError("LeafNode has no value")
+        if self.tag is None:
+            return str(self.value)
+
+        properties = ""
+        if self.props is not None:
+            for k, val in self.props.items():
+                properties += f" {k}=\"{val}\""
+        return f"<{self.tag}{properties}>{self.value}</{self.tag}>"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag : str, children: list[HTMLNode], props=None) -> None:
+        super().__init__(tag, children=children, props=props)
+
+    def to_html(self) -> str:
+        if self.tag is None:
+            raise ValueError("parent node has no tag")
+        if self.children is None:
+            raise ValueError("parent node has no children")
+
+        html = f"<{self.tag}{self.props_to_html()}>"
+        for c in self.children:
+            html += c.to_html()
+        html += f"</{self.tag}>"
+
+        return html
+
+    def __repr__(self) -> str:
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
